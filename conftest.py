de349bbc93eb567c11a43f27f44ad7bc6e4d2f46
Michael KUO <ufo00816@gmail.com>
2025-05-09 16:37:51 +0800

    fix_emulator_setting

--- conftest.py
+++ conftest.py
@@ -1,48 +1,65 @@
-from email.policy import default
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as chrome_options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 def pytest_addoption(parser):
-    parser.addoption("--MobileEmulation", action="store", default="iPhone 12 Pro",
-                     choices=("iPhone 12 Pro", False), help="Testing in MobileEmulation or not")
+    parser.addoption("--MobileEmulation", action="store", default="iPhone 14 Pro Max",
+                     choices=("iPhone 14 Pro Max", False), help="Testing in MobileEmulation or not. Options: True/False")
     parser.addoption("--browser", action='store', default="chrome",
-                   choices=("chrome", "Chrome"), help="Choice the test platform")
-    parser.addoption("--windows-size", action='store', default="390,844",
-                   help="emulation size")
+                   choices=("chrome", "Chrome"), help="Choice the test platform, Options: Chrome")
+    parser.addoption("--windows-size", action='store', default="344,746",
+                   help="emulation size, Option: 'Width: x pixel, Length: y pixel', e.g.: '430,932'")
     parser.addoption("--headless", action='store', default=False,
                    choices=(True, False), help="Headless mode?")
+    parser.addoption("--env", action='store', default="Stage",
+                   choices=("stage", "Stage", "UAT", "uat", "dev", "Dev"), help="Headless mode?")
+    parser.addoption("--maxscreen", action='store', default=True,
+                   choices=(True, False), help="Max Screen? Options: True/False")
 
 @pytest.fixture(scope="session")
-def driver_options_modifier(request) -> Options:
+def driver_options_modifier(request):
     """
     Take command line parameter by leverage pytest_addoption() to alter the driver options
     :param request: pytest build-in parameters passed by fixture. Do nothing with it
     :return: Options for drivers to modify browser behaviour
     """
-    options = Options()
-    options.add_argument("--no-sandbox")
-    options.add_experimental_option("useAutomationExtension", False)
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    browser = request.config.getoption("--browser").lower()
 
-    mobile_emulation_test =  request.config.getoption("--MobileEmulation")
-    window_size = request.config.getoption("--windows-size")
-    headless = request.config.getoption("--headless")
+    match browser:
+        case "chrome" | _:
+            options = chrome_options()
 
-    if mobile_emulation_test:
-        options.add_experimental_option("mobileEmulation", {"deviceName": mobile_emulation_test})
 
-    if window_size:
-        options.add_argument(window_size)
+            options.add_argument("--no-sandbox")
+            options.add_experimental_option("useAutomationExtension", False)
+            options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
-    if headless:
-        options.add_argument("headless=new")
+            mobile_emulation_test =  request.config.getoption("--MobileEmulation")
+            window_size = request.config.getoption("--windows-size").replace(" ", "").split(",", 1)
 
-    return options
+            headless = request.config.getoption("--headless")
+
+
+            # reference for Device information: https://github.com/alxwndr/list-of-custom-emulated-devices-in-chrome
+
+            mobile_emulation = {
+                "deviceMetrics": {"width": int(window_size[0]), "height": int(window_size[1]), "pixelRatio": 3},
+                "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19",
+                "clientHints": {"platform": "iOS", "mobile": True}}
+
+            if mobile_emulation_test:
+                options.add_experimental_option("mobileEmulation",  mobile_emulation)
+
+            if window_size:
+                options.add_argument(f"--windows-size={window_size[0]},{window_size[1]}")
+
+            if headless:
+                options.add_argument("headless=new")
+
+            return options
 
 @pytest.fixture(scope="function")
 def driver(request, driver_options_modifier) -> webdriver:
--- tests/test_twitch.py
+++ tests/test_twitch.py
@@ -4,12 +4,8 @@ import pytest
 
 class TestTwitch:
 
+    @pytest.mark.twitch
     def test_open_home_page(self, driver):
         twitch_home_page = HomePage(driver)
-        twitch_home_page.open("https://m.twitch.tv/")
-        pass
-
-    def test_open_home_page_2(self, driver):
-        twitch_home_page = HomePage(driver)
-        twitch_home_page.open("https://m.twitch.tv/")
+        twitch_home_page.open("https://twitch.tv/")
         pass
